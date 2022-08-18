5932b8e66a7515789db9abed99dff33992903346
Tatiana <k.t.s-92@yandex.ru>
2022-08-18 20:25:15 +0700

    yea

---
+++ q1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = b;
+if (max < b)
+{
+    max = b;
+    min = a;
+}
+if (max > b) min = b;
+Console.Write("min = ");
+Console.WriteLine( min );
+Console.Write( "max = " );
+Console.WriteLine( max );
