c6f7655bae50ad49f9baae5436e6f8ae7716f887
Tatiana <k.t.s-92@yandex.ru>
2022-08-19 10:18:07 +0700

    homework 2.3 week number

--- q3/Program.cs
+++ q3/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Coonsole.Write("Введите число от 1 до 7");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a ==7 | a == 6) Console.WriteLine("Yes, holyday number");
+else {Console.WriteLine("No, shvah-day number")};
