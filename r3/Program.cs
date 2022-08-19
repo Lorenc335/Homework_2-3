b4b76a046e50fc2553e07da1805aae76d625fd92
Tatiana <k.t.s-92@yandex.ru>
2022-08-19 22:59:07 +0700

    homework 3.3

---
+++ r3/Program.cs
@@ -0,0 +1,14 @@
+﻿int Main(int i)
+{
+    int d = i*i*i;
+    return d;
+}
+Console.Write("введите число :");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i<n+1)
+{
+    Console.Write(Main(i));
+    Console.Write(' ');
+    i = i + 1;
+}
