33ddfbad05047a50303f96415187814c7370c27f
Tatiana <k.t.s-92@yandex.ru>
2022-08-19 20:36:01 +0700

    fixed errors

--- q3/Program.cs
+++ q3/Program.cs
@@ -1,4 +1,4 @@
-﻿Coonsole.Write("Введите число от 1 до 7");
+﻿Console.Write("Введите число от 1 до 7");
 int a = Convert.ToInt32(Console.ReadLine());
 if (a ==7 | a == 6) Console.WriteLine("Yes, holyday number");
-else {Console.WriteLine("No, shvah-day number")};
+else {Console.WriteLine("No, shvah-day number");}
---
+++ r1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите пятизначное число :");
+int a = Convert.ToInt32(Console.ReadLine());
+int x = a/10000;
+int z = a%10;
+if (x == z)
+{
+    a = a - (x * 10000 + z);
+    x = a / 1000;
+    z = (a % 100) / 10;
+    if (x == z) Console.WriteLine("Azor roza; Yes");
+    else {Console.WriteLine("Kabanchik, no boku; No");}
+}
+else {Console.WriteLine("Kabanchik, no boku; No");}
