dda51413413cc95c4816d623c4cc921782ca1a52
Tatiana <k.t.s-92@yandex.ru>
2022-08-19 10:05:49 +0700

    homework 2.2

--- q2/Program.cs
+++ q2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100) Console.WriteLine("В полученном числе нет третьего символа");
+if (a> 1000) a = a/ 10 
+else
+{
+    {
+        a = a% 10;
+        Console.Write("В полученном числе третий символ равен ");
+        Console.WritwLine(a);
+    }
+}
