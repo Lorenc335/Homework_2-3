32a9453f008f74f5c33dc8b239ae39e8b103a00f
Tatiana <k.t.s-92@yandex.ru>
2022-08-21 01:44:37 +0700

    homework 4.1

---
+++ s1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите a : ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b : ");
+int b = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int s = 1;
+Console.Write("И получите МАГИЮ - ");
+while (count < b)
+{
+    s = s * a;
+count ++;
+}
+Console.WriteLine(s);
