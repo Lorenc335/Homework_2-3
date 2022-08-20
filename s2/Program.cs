c8696c91b45a3891dc863e4ac8947d206dc0b3ed
Tatiana <k.t.s-92@yandex.ru>
2022-08-21 02:09:42 +0700

    homework 4.2

---
+++ s2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите нормальное число ( больше двух знаков ): ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+int x = 0;
+while (a > 0)
+{
+    b = a % 10;
+    x = x + b;
+    a = a / 10;
+}
+Console.Write("Программа сочла нужным сложить каждый знак числа и вышло -> ");
+Console.WriteLine(x);
