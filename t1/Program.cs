4ccb0a01e3150fe12140003ff488dc06735b110b
Tatiana <k.t.s-92@yandex.ru>
2022-08-25 19:08:54 +0700

    homework 5.1

---
+++ t1/Program.cs
@@ -0,0 +1,25 @@
+﻿static int[] CreateArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+    return array;
+}
+
+static int chetnoe(int[] array)
+{
+    int k = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) k ++;
+    }
+    return k;
+}
+Console.Write("Введите число аргументов массива : ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Массив : ");
+Console.WriteLine(CreateArray(n));
+Console.Write("Массив содержит четных аргументов - ");
+Console.WriteLine(chetnoe(CreateArray(n)));
