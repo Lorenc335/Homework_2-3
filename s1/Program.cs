3294986aa0102eb590c0180c15c72a367b46af9f
Tatiana <k.t.s-92@yandex.ru>
2022-08-25 16:00:54 +0700

    change on method

--- s1/Program.cs
+++ s1/Program.cs
@@ -1,13 +1,16 @@
-﻿Console.Write("Введите a : ");
+﻿static int stepen(int a, int b)
+{
+    int count = 0;
+    int s = 1;
+    while (count < b)
+    {
+        s = s * a;
+        count ++;
+    }
+    return s;
+}
+Console.Write("Введите a : ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите b : ");
 int b = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-int s = 1;
-Console.Write("И получите МАГИЮ - ");
-while (count < b)
-{
-    s = s * a;
-count ++;
-}
-Console.WriteLine(s);
+Console.WriteLine(stepen(a, b));
