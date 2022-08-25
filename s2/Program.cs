9ed533543da3ce40e298da103c971b5f27d35b9d
Tatiana <k.t.s-92@yandex.ru>
2022-08-25 16:01:52 +0700

    change on method

--- s2/Program.cs
+++ s2/Program.cs
@@ -1,12 +1,16 @@
-﻿Console.Write("Введите нормальное число ( больше двух знаков ): ");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = 0;
-int x = 0;
-while (a > 0)
+﻿static int summa(int a)
 {
-    b = a % 10;
-    x = x + b;
-    a = a / 10;
+    int b = 0;
+    int x = 0;
+    while (a > 0)
+    {
+        b = a % 10;
+    x =     x + b;
+        a = a / 10;
+    }
+    return x;
 }
+Console.Write("Введите нормальное число ( больше двух знаков ): ");
+int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Программа сочла нужным сложить каждый знак числа и вышло -> ");
-Console.WriteLine(x);
+Console.WriteLine(summa(a));
