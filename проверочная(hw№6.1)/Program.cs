6c0256b4b02d226a0bc494e4fb3f67b270b34651
Tatiana <k.t.s-92@yandex.ru>
2022-09-03 10:46:58 +0700

    add homework 6

---
+++ проверочная(hw№6.1)/Program.cs
@@ -0,0 +1,23 @@
+﻿static int[] ArrayRandom()
+{
+    int[] array = new int[123];
+    for (int i = 0; i < 123; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+static int Args1099(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < 123; i++)
+    {
+        if (array[i] == 10) count++;
+        if (array[i] == 99) count++;
+    }
+    return count;
+}
+
+Console.Write("количество аргументов со значением 10 и 99 в массиве равно ");
+Console.WriteLine(Args1099(ArrayRandom()));
---
+++ проверочная(hw№6.2)/Program.cs
@@ -0,0 +1,26 @@
+﻿static int[] Array()
+{
+    int[] array = new int[10];
+    for (int i = 0; i < 10; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+    return array;
+}
+
+static int[] SumFirstFinish(int[] array)
+{
+    int n = 10;
+    int[] newarray = new int[10];
+    for (int i = 0; i < n; i++)
+    {
+        newarray[i] = (array[i] + array[n - 1]);
+        n--;
+        Console.Write(newarray[i]);
+        Console.Write(", ");
+    }
+    return newarray;
+}
+
+Console.WriteLine("Данный массив создан посредством сложения пар аргументов первого массива");
+SumFirstFinish(Array());
