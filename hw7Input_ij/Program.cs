7cd4e4c776f04496a85ff465772193d9266ca2c1
Tatiana <k.t.s-92@yandex.ru>
2022-09-08 08:32:33 +0700

    homework 7.2

---
+++ hw7Input_ij/Program.cs
@@ -0,0 +1,34 @@
+﻿static int[, ] Matrix()
+{
+    int[, ] matrix = new int[5, 5];
+     return FillMatrix(matrix);
+}
+
+static int[, ] FillMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j< matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-9, 9);
+            Console.Write(matrix[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+static void Index(int[, ] matrix)
+{
+    Console.Write("Введите первое число двумерной координаты - ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число двумерной координаты - ");
+    int y = Convert.ToInt32(Console.ReadLine());
+    if (x > 5 | y > 5) Console.WriteLine("Такой координаты не найдено ");
+    else 
+    {
+        Console.Write("Выбранная координата содержит число - ");
+        Console.WriteLine(matrix[x - 1, y - 1]);
+    }
+}
+Index(Matrix());
---
+++ hw7UserMatrix/Program.cs
@@ -0,0 +1,23 @@
+﻿static void MatrixOfUser()
+{
+    Console.Write("Введите число строк для построения матрицы - ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число столбцов - ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    double[, ] matrix = new double[n, m];
+    FillMatrix(matrix);
+}
+
+static void FillMatrix(double[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 9);
+            Console.Write(matrix[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+MatrixOfUser();
