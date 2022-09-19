6f3a9a158130f3c5b482287e9189058373bc8e76
Tatiana <k.t.s-92@yandex.ru>
2022-09-19 13:40:11 +0700

    homework 9.2

---
+++ hw9NaturSum/Program.cs
@@ -0,0 +1,22 @@
+﻿static int NaturNumAdd(int n, int m)
+{
+    if (n == m) return m;
+    else
+    {
+        return n + NaturNumAdd(n + 1, m);
+    }
+}
+
+Console.Write("Введите первое число = ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число = ");
+int m = Convert.ToInt32(Console.ReadLine());
+int tess;
+if (n > m)
+{
+    tess = n;
+    n = m;
+    m = tess;
+}
+Console.WriteLine("Программа выводит сумму всех натуральных чисел промежутка [n, m]");
+Console.WriteLine(NaturNumAdd(n, m));
