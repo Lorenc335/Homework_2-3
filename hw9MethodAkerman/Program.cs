26577a9aa2580776358c55a4c78defe33784df10
Tatiana <k.t.s-92@yandex.ru>
2022-09-19 22:29:58 +0700

    homework 9.3

---
+++ hw9MethodAkerman/Program.cs
@@ -0,0 +1,15 @@
+﻿static int A(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else
+        if ((n != 0) && (m == 0)) return A(n - 1, 1);
+    else
+        return A(n - 1, A(n, m - 1));
+}
+Console. WriteLine("Функция Акермана.");
+Console.WriteLine("(человечным явлением будет - вводить цифры в диапазоне [0, 3])");
+Console.Write("Введите n -");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите m - ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(A(n, m));
