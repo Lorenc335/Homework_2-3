b3e49e7bb989e981ba2a8a2c3afb0ab375e87e2d
Tatiana <k.t.s-92@yandex.ru>
2022-09-19 13:38:40 +0700

    homework 9.1

---
+++ hw9NaturNum/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Программа выводит все натуральные числа в промежутке [n, m]");
+Console.Write("Введите n : ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите m : ");
+int m = Convert.ToInt32(Console.ReadLine());
+int tess ;
+if (n > m)
+{
+    tess = n;
+    n = m;
+    m = tess;
+}
+for (int i = n; i < m + 1; i++)
+{
+    Console.Write( i );
+    Console.Write(" ");
+}
